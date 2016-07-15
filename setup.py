a0b010668546ff3d5eb25612b484632093893fe8
Sean Harrington <SeanH@nep01s-MacBook-Pro.local>
2016-07-15 01:28:50 -0400

    Added version of dependencies to 'install_requires'

--- setup.py
+++ setup.py
@@ -9,5 +9,12 @@ setup(
         url='https://github.com/seanharr11/etlalchemy',
         download_url='https://github.com/seanharr11/etlalchemy/tarball/0.1',
         keywords=['sql','migration','etl','database'],
+        install_requires = [
+            "py == 1.4.31",
+            "six == 1.9.0",
+            "SQLAlchemy == 1.0.13",
+            "sqlalchemy-migrate == 0.9.7",
+            "SQLAlchemy-Utils == 0.30.9"
+        ],
         classifiers=[],
 )
