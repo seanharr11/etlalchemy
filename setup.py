798700d630bf9bae0f4a53684de726a928414314
Sean Harrington <SeanH@nep01s-MacBook-Pro.local>
2016-07-14 23:58:01 -0400

    Merging 'performance' into master

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import steup
+setup(
+        name = 'etlalchemy',
+        packages = ['etlalchemy'],
+        version = '0.1',
+        description = 'Extract, Transform, Load. Migrate any SQL Database in 4 lines of code',
+        author = 'Sean Harrington',
+        author_email='seanharr11@gmail.com',
+        url='https://github.com/seanharr11/etlalchemy',
+        download_url='https://github.com/seanharr11/etlalchemy/tarball/0.1',
+        keywords=['sql','migration','etl','database'],
+        classifiers=[],
+)
